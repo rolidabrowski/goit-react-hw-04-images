7d6d70d0f292e0c5d51f30e1df34aa525befb3de
Roland Dąbrowski <roli.dabrowski@gmail.com>
2023-06-24 19:51:25 +0200

    refacor App using hooks

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,24 +1,19 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import { Searchbar } from '../Searchbar';
 import { ImageGallery } from '../ImageGallery';
 import css from './App.module.css';
 
-export class App extends Component {
-  state = {
-    searchQuery: '',
+export const App = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleSubmit = searchQuery => {
+    setSearchQuery(searchQuery);
   };
 
-  handleSubmit = searchQuery => {
-    this.setState({ searchQuery });
-  };
-
-  render() {
-    const { searchQuery } = this.state;
-    return (
-      <div className={css.App}>
-        <Searchbar onSubmit={this.handleSubmit} />
-        <ImageGallery value={searchQuery} />
-      </div>
-    );
-  }
-}
+  return (
+    <div className={css.App}>
+      <Searchbar onSubmit={handleSubmit} />
+      <ImageGallery value={searchQuery} />
+    </div>
+  );
+};
